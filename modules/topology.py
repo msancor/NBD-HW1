94edc883eb9c9ca484cab801072404727476bf65
pltommasino <pltommasino@gmail.com>
2024-05-14 00:22:11 +0200

    Final version, missed Fat-tree description

--- modules/topology.py
+++ modules/topology.py
@@ -1,4 +1,5 @@
 import numpy as np
+import networkx as nx
 
 
 def response_time_func(C, tau, L_f, L_o, E_X, T_0, f, N, h):
@@ -43,3 +44,39 @@ def response_time_func(C, tau, L_f, L_o, E_X, T_0, f, N, h):
     response_time = np.max(r_t)
     
     return(response_time, theta)
+
+
+def construct_jellyfish_topology(switches, ports, r, n_servers) -> nx.Graph:
+    """
+    Build a Jellyfish topology graph.
+
+    Args:
+        switches: The number of switches in the Jellyfish topology.
+        ports: The number of ports per switch.
+        r: The number of neighbor switches to connect to.
+        n_servers: The total number of servers to add to the topology.
+
+    Returns:
+        A Jellyfish topology graph with the specified parameters.
+    """
+
+    #Create switch structure
+    G = nx.random_regular_graph(r, switches)
+
+    #Label switches with 0
+    for i in range(switches):
+        G.nodes[i]['type'] = 0 
+    #Add servers labeling them as 1
+    H = nx.Graph()
+    H.add_nodes_from(G.nodes(data=True))
+    H.add_edges_from(G.edges())
+    servers_counter = 0
+    for i in range(switches):
+        for j in range(ports - r):
+            if servers_counter <= n_servers:
+                server_name = f'server_{i}_{j}'
+                H.add_node(server_name)
+                H.nodes[server_name]['type'] = 1
+                H.add_edge(i, server_name)
+                servers_counter += 1 
+    return H
